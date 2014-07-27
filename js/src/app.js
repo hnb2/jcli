736f908ddb2e6806c5adb7d2e5a09e1a43c3b011
Pierre Guillemot <pierreguillemot@yahoo.fr>
2014-07-27 18:36:56 +0800

    First simple demo without events

--- js/src/app.js
+++ js/src/app.js
@@ -1,10 +1,35 @@
 /**
  * Demo to present JCli
+ * @class Demo
  * @author Pierre Guillemot
  */
 define(['jcli'], function (JCli) {
+    "use strict";
 
+    //Initialize JCLI, no context here
     var jcli = new JCli();
-    console.log('JCli is set');
+
+    //Create a simple hello command
+    var hello = {
+        'name': 'hello',
+        'description': 'Just say hello.',
+        'params': [
+            {
+                'name': 'name',
+                'type': 'string'
+            }
+        ],
+        'exec': function (args, context) {
+            return 'Hello ' + args[0];
+        }
+    };
+
+    //Add it to our command registry
+    jcli.add_command(hello);
+
+    //Try to interpret the following:
+    jcli.interpret('hello shawn');
+    jcli.interpret('hell0 shawn');
+    jcli.interpret('hello');
 
 });
