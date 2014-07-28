a4cd7441b2065848423e2b1e36cc21a4af965c4f
Pierre Guillemot <pierreguillemot@yahoo.fr>
2014-07-28 22:51:57 +0800

    Updated the demo

--- demo/app.js
+++ demo/app.js
@@ -5,9 +5,35 @@
  */
 require(['jcli', 'jquery'], function (JCli, $) {
     "use strict";
+ 
+    //When a command has been successfully executed, append it to
+    // the main div.
+    var successful_execution = function (_result) {
+        var success = $('<div>').attr(
+            'class',
+            'cli-entry'
+        ).text(_result);
+        $('#cli').append(success);
+    };
+
+    //When a command has failed, do the same but add a class 'error'
+    // to print the message in red.
+    var failed_execution = function (_error) {
+        var failure = $('<div>').attr(
+            'class',
+            'cli-entry error'
+        ).text(_error.message);
+        $('#cli').append(failure);
+    };
+
+    var options = {
+        context: {},
+        on_success: successful_execution,
+        on_failure: failed_execution
+    };
 
     //Initialize JCLI, no context here
-    var jcli = new JCli();
+    var jcli = new JCli(options);
 
     //Create a simple hello command
     var hello = {
@@ -26,26 +52,6 @@ require(['jcli', 'jquery'], function (JCli, $) {
 
     //Add it to our command registry
     jcli.add_command(hello);
-
-    //When a command has been successfully executed, append it to
-    // the main div.
-    jcli.successful_execution = function (_result) {
-        var success = $('<div>').attr(
-            'class',
-            'cli-entry'
-        ).text(_result);
-        $('#cli').append(success);
-    };
-
-    //When a command has failed, do the same but add a class 'error'
-    // to print the message in red.
-    jcli.failed_execution = function (_result) {
-        var failure = $('<div>').attr(
-            'class',
-            'cli-entry error'
-        ).text(_result);
-        $('#cli').append(failure);
-    };
 
     //When clicking on 'Submit', interpret the command
     $('#submit').on('click', function () {
