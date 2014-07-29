95534a9cfabd3fb0957b38be45c8b8800bb98a94
Pierre Guillemot <pierreguillemot@yahoo.fr>
2014-07-29 21:53:45 +0800

    Added the tasks requirejs + copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,39 @@ module.exports = function (grunt) {
                     outdir: 'build/doc'
                 }
             }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: 'js/src',
+                    paths: {
+                        lib: '../lib'
+                    },
+                    name: 'jcli',
+                    out: 'build/jcli.js'
+                }
+            }
+        },
+        copy: {
+            main: {
+                src: 'build/jcli.js',
+                dest: 'demo/jcli.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-yuidoc');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
+    /**
+     * Generate the documentation using YuiDoc
+     */
     grunt.registerTask('docs', ['yuidoc']);
+
+    /**
+     * Build the project jcli.js into build/
+     * and copy it into demo/
+     */
+    grunt.registerTask('build', ['requirejs', 'copy']);
 };
