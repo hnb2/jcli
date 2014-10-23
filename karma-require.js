2baba21dee72413ace7f0cb00e2d398e99563f9e
Pierre Guillemot <pierreguillemot@yahoo.fr>
2014-10-23 20:55:00 +0800

    re #1 : Fixed the lib path when running unit tests

--- karma-require.js
+++ karma-require.js
@@ -1,24 +1,19 @@
 var allTestFiles = [];
 var TEST_REGEXP = /test.*\.js$/i;
 
-var pathToModule = function (path) {
-    return path.replace(/^\/base\//, '').replace(/\.js$/, '');
-};
-
 Object.keys(window.__karma__.files).forEach(function (file) {
-
     if (TEST_REGEXP.test(file)) {
-        // Normalize paths to RequireJS module names.
-        var path = pathToModule(file);
-
-        //TODO: we are not normalizing path anymore
         allTestFiles.push(file);
     }
 });
 
 require.config({
     // Karma serves files under /base, which is the basePath from your config file
-    baseUrl: '/base',
+    baseUrl: '/base/js/src',
+
+    paths: {
+        'lib': '../lib'
+    },
 
     // dynamically load all test files
     deps: allTestFiles,
